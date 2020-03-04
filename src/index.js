9860e6a484c8174cb7259b3b9be9c63d2a7e4560
Zhouchanglong <changlong.zhou@56qq.com>
2020-03-04 23:08:43 +0800

    feat: 添加webpack基础配置以及babel配置

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-const greeting = 'just a tutorial is going';
+import styles from './style.less';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import layers from './public/layers.png';
+// import className from 'classnames';
+
+const App = () => (<>
+  <div className={styles.foo}>React App</div>
+  <img src={layers} />
+</>);
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,97 @@
-module.export = {};
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    // 静态资源存放地址
+    publicPath: '/'
+  },
+  mode: 'development',
+  devServer: {
+    port: 3060,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        use: 'babel-loader?compact=false'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif)/i,
+      //   use: {
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[ext]',
+      //       outputPath: 'public/images/',
+      //     }
+      //   }
+      // },
+      {
+        test: /\.(jpe?g|png|gif)/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'public/images/',
+            limit: 8192,
+            // limit: 381920,
+            // fallback: require.resolve('file-loader')
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-boilerplate',
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new CleanWebpackPlugin(),
+  ]
+};
